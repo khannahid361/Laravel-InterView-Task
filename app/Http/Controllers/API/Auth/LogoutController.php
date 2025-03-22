41aed27a287db9e074e275075f9575148a652f85
nahid361 <khannahid361@gmail.com>
2025-03-22 20:43:52 +0600

    Logout Done

--- app/Http/Controllers/API/Auth/LogoutController.php
+++ app/Http/Controllers/API/Auth/LogoutController.php
@@ -7,5 +7,16 @@ use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
+    public function __invoke(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\API\Auth\loginController;
+use App\Http\Controllers\API\Auth\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login',[loginController::class, '__invoke']);
-Route::post('/auth/logout',[loginController::class, '__invoke']);
+Route::post('/auth/logout',[LogoutController::class, '__invoke']);
 Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
