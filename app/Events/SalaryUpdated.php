b6457ade32bb9d60fda6d83c6a2faaaaa8342f10
nahid361 <khannahid361@gmail.com>
2025-03-24 21:57:09 +0600

    Updated

--- app/Events/SalaryUpdated.php
+++ app/Events/SalaryUpdated.php
@@ -26,11 +26,6 @@ class SalaryUpdated
         $this->newSalary = $newSalary;
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
     public function broadcastOn(): array
     {
         return [
